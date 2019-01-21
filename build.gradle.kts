a5a0d2953d0e8b9a878023c2b79127d07a92fed2
Grzegorz Ćwiak <grzegorz.cwiak@crosswordcybersecurity.com>
2019-01-21 22:08:28 +0100

    bloom-lib: NIX-442 Convert Gradle script to Kotlin DSL, apply java-config-plugin and type-safe dependencies config.
    Branch: feature/NIX-442_convert_simple_Gradle_scripts_to_Kotlin_DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 import buildConfig.libraries
-import eu.xword.gradle.plugins.JavaConfigPlugin
 
-apply plugin: JavaConfigPlugin
-
-dependencies {
-    compile libraries.guava
-    compile libraries.jackson.annotations
-    compile libraries.jackson.core
-    compile libraries.jackson.databind
+plugins {
+    id("java-config-plugin")
 }
 
+dependencies {
+    compile(libraries.guava)
+    compile(libraries.jackson.annotations)
+    compile(libraries.jackson.core)
+    compile(libraries.jackson.databind)
+}
