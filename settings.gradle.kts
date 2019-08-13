cbd74d432566679a38216c135a03a84e1b3a4980
Kamil Wojcik <kamil.wojcik@crosswordcybersecurity.com>
2019-08-13 17:08:31 +0200

    - make gradle build script file names customizable - add auto detection of gradle subprojects

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,5 +5,29 @@ pluginManagement {
 }
 rootProject.name = "nixer-spring-plugin"
 
-include("example", "core", "example-light")
+// Below code auto-detects subprojects and include them with name based and build filename, ex. `example.build.kts` becomes :example.
 
+var buildFiles: ConfigurableFileTree = fileTree(rootDir) {
+    include("**/*.gradle.kts")
+    exclude("build", "**/gradle", "settings.gradle.kts", "buildSrc", "/build.gradle.kts", ".*", "out")
+    exclude("**/grails3")
+}
+
+var rootDirPath = rootDir.absolutePath + File.separator
+buildFiles.forEach { buildFile: File ->
+
+    val isDefaultName = "build.gradle.kts".equals(buildFile.name)
+    if(isDefaultName) {
+        val buildFilePath = buildFile.parentFile.absolutePath
+        val projectPath = buildFilePath.replace(rootDirPath, "").replace(File.separator, ":")
+        include(projectPath)
+    } else {
+        val projectName = buildFile.name.replace(".gradle.kts", "");
+        val projectPath = ":" + projectName;
+        include(projectPath)
+        val project = findProject(projectPath)
+        project?.name = projectName
+        project?.projectDir = buildFile.parentFile
+        project?.buildFileName = buildFile.name
+    }
+}
