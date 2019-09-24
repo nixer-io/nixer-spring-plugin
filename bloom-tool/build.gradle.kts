0b7110c9cf628a96df5c33b2cea6869ab3405a3f
Grzegorz Ćwiak <grzegorz.cwiak@crosswordcybersecurity.com>
2019-09-24 22:43:08 +0200

    - add junit-vintage-engine dependency to bloom-tool so Gradle runs both junit4 tests as well.

--- bloom-tool/build.gradle.kts
+++ bloom-tool/build.gradle.kts
@@ -8,9 +8,11 @@ dependencies {
     implementation("com.offbytwo:docopt:0.6.0.20150202")    // CLI parsing, see: https://github.com/docopt/docopt.java
     implementation("com.google.guava:guava:22.0")
 
-    testImplementation("junit:junit:4.12") // TODO junit5
+    testImplementation("junit:junit:4.12")
     testImplementation("pl.pragmatists:JUnitParams:1.0.6")
     testImplementation("org.assertj:assertj-core:3.6.2")
+
+    testRuntimeOnly("org.junit.vintage", "junit-vintage-engine","5.3.2")
 }
 
 //rootProject.tasks.getByName<Sync>("copyDistribution") {
