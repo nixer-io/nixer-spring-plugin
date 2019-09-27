6c9fed06f7c7643fbb01471a312404d3574cc8b6
Grzegorz Ćwiak <grzegorz.cwiak@crosswordcybersecurity.com>
2019-09-27 15:54:19 +0200

    - convert the CLI test class to kotlin

--- bloom-tool/src/test/kotlin/eu/xword/nixer/bloom/cli/BloomToolMainTest.kt
+++ bloom-tool/src/test/kotlin/eu/xword/nixer/bloom/cli/BloomToolMainTest.kt
@@ -1,40 +1,39 @@
-package eu.xword.nixer.bloom.cli;
+package eu.xword.nixer.bloom.cli
 
-import java.io.File;
-import java.io.IOException;
-
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import java.io.File
+import java.io.IOException
 
 /**
  * Created on 23/08/2018.
  *
  * @author cezary
  */
-public class BloomToolMainTest {
+class BloomToolMainTest {
 
     @Rule
-    public final TemporaryFolder temporaryFolder = new TemporaryFolder();
+    @JvmField
+    val temporaryFolder = TemporaryFolder()
 
     @Test
-    public void createFilter() throws IOException {
+    fun `should create filter`() {
         // given
-        final File file = temporaryFolder.newFile("test.bloom");
-        delete(file);
+        val file = temporaryFolder.newFile("test.bloom")
+        delete(file)
 
         // when
-        BloomToolMainKt.main(new String[]{"create", "--size=100", "--fpp=1e-2", file.getAbsolutePath()});
+        main(arrayOf("create", "--size=100", "--fpp=1e-2", file.absolutePath))
 
         // then
-        assertThat(file).exists();
+        assertThat(file).exists()
     }
 
-    private static void delete(final File file) throws IOException {
+    private fun delete(file: File) {
         if (!file.delete()) {
-            throw new IOException("Failed to delete: " + file);
+            throw IOException("Failed to delete: $file")
         }
     }
 }
