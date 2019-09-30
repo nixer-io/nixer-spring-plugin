8fa271d59bd38898d6c2d1d430200cfda45d781b
Kamil Wojcik <kamil.wojcik@crosswordcybersecurity.com>
2019-09-30 18:15:45 +0200

    - add draft of readme for ip ranges and cloud-ip-ranges script  - refactor cloud-ip-ranges

--- scripts/ip_cloud_ranges/cloud-ip-ranges.py
+++ scripts/ip_cloud_ranges/cloud-ip-ranges.py
@@ -15,9 +15,6 @@ Options:
   -v --verbose      Verbose mode.
 """
 import requests
-from requests.exceptions import HTTPError
-import functools
-import operator
 import subprocess
 import json
 from datetime import datetime
@@ -25,46 +22,25 @@ from docopt import docopt
 from bs4 import BeautifulSoup
 import re
 
-def fetchPage(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
+def fetch_page(url):
+    response = requests.get(url)
+    response.raise_for_status()
 
-        return response.content
-    except HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')  # Python 3.6
-    except Exception as err:
-        print(f'Other error occurred: {err}')  # Python 3.6
-    else:
-        print('Success!')
+    return response.content
 
 
-def fetchJson(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
+def fetch_json(url):
+    response = requests.get(url)
+    response.raise_for_status()
 
-        return response.json()
-    except HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')  # Python 3.6
-    except Exception as err:
-        print(f'Other error occurred: {err}')  # Python 3.6
-    else:
-        print('Success!')
+    return response.json()
 
 
-def fetchText(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
+def fetch_text(url):
+    response = requests.get(url)
+    response.raise_for_status()
 
-        return response.text
-    except HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')  # Python 3.6
-    except Exception as err:
-        print(f'Other error occurred: {err}')  # Python 3.6
-    else:
-        print('Success!')
+    return response.text
 
 
 def unique_sorted(elements):
@@ -76,18 +52,12 @@ def unique_sorted(elements):
 
 
 # https://docs.aws.amazon.com/general/latest/gr/aws-ip-ranges.html
-def fetchAwsIpRanges():
+def fetch_aws_ip_ranges():
     url = 'https://ip-ranges.amazonaws.com/ip-ranges.json'
-    print("Fetching IP ranges from AWS")
-    ips = fetchJson(url)
+    ips = fetch_json(url)
 
     ipv4_ranges = [ prefix["ip_prefix"] for prefix in ips["prefixes"] ]
     ipv6_ranges = [ prefix["ipv6_prefix"] for prefix in ips["ipv6_prefixes"] ]
-
-    print(f'Found prefixes IPv4: {len(ipv4_ranges)} IPv6: {len(ipv6_ranges)}')
-
-    ipv4_ranges = unique_sorted(ipv4_ranges)
-    ipv6_ranges = unique_sorted(ipv6_ranges)
 
     return {
         "name": "aws",
@@ -116,46 +86,33 @@ def fetchAwsIpRanges():
 #   ]
 # }
 # https://docs.microsoft.com/en-us/azure/virtual-network/public-ip-address-prefix
-def fetchAzureIpRanges():
+# Please note that link to ip ranges resource is dynamically changing.
+def fetch_azure_ip_ranges():
     url = 'https://download.microsoft.com/download/7/1/D/71D86715-5596-4529-9B13-DA13A5DE5B63/ServiceTags_Public_20190909.json'
-    print("Fetching IP ranges from Azure")
-    ips = fetchJson(url)
+    ips = fetch_json(url)
 
     ipv4_ranges = []
     for service in ips["values"]:
         prefixes = service["properties"]["addressPrefixes"]
         ipv4_ranges.extend(prefixes)
 
-    ipv6_ranges = []
-
-    print(f'Found prefixes IPv4: {len(ipv4_ranges)} IPv6: {len(ipv6_ranges)}')
-
-    ipv4_ranges = unique_sorted(ipv4_ranges)
-    ipv6_ranges = unique_sorted(ipv6_ranges)
-
     return {
         "name": "Azure",
         "ipv4_prefixes": ipv4_ranges,
-        "ipv6_prefixes": ipv6_ranges
+        "ipv6_prefixes": []
     }
 
 
 # https://www.cloudflare.com/ips/
-def fetchCloudflareIpRanges():
+def fetch_cloudflare_ip_ranges():
     ipv4_url = 'https://www.cloudflare.com/ips-v4'
     ipv6_url = 'https://www.cloudflare.com/ips-v6'
 
-    print("Fetching Ip from Cloudflare")
-    ipv4_text = fetchText(ipv4_url)
-    ipv6_text = fetchText(ipv6_url)
+    ipv4_text = fetch_text(ipv4_url)
+    ipv6_text = fetch_text(ipv6_url)
 
     ipv4_ranges = [ line for line in ipv4_text.splitlines() ]
     ipv6_ranges = [ line for line in ipv6_text.splitlines() ]
-
-    print(f'Found prefixes IPv4: {len(ipv4_ranges)} IPv6: {len(ipv6_ranges)}')
-
-    ipv4_ranges = unique_sorted(ipv4_ranges)
-    ipv6_ranges = unique_sorted(ipv6_ranges)
 
     return {
         "name": "cloudflare",
@@ -169,9 +126,7 @@ def run(commands):
 
 
 # Google https://cloud.google.com/compute/docs/faq#find_ip_range
-def fetchGceIpRanges():
-    print("Fetching IP ranges from GCE")
-
+def fetch_gce_ip_ranges():
     output = str(run(["dig", "@8.8.8.8", "-t", "TXT", "_cloud-netblocks.googleusercontent.com"]).stdout)
 
     answer = [line for line in output.splitlines() if "v=spf1" in line]
@@ -190,12 +145,6 @@ def fetchGceIpRanges():
         ipv6_prefixes = [ token.replace("ip6:", "") for token in tokens if token.startswith("ip6:") ]
         ipv6_ranges.extend(ipv6_prefixes)
 
-
-    print(f'Found prefixes IPv4: {len(ipv4_ranges)} IPv6: {len(ipv6_ranges)}')
-
-    ipv4_ranges = unique_sorted(ipv4_ranges)
-    ipv6_ranges = unique_sorted(ipv6_ranges)
-
     return {
         "name": "gce",
         "ipv4_prefixes": ipv4_ranges,
@@ -204,38 +153,26 @@ def fetchGceIpRanges():
 
 
 # IBM https://cloud.ibm.com/docs/infrastructure/virtual-router-appliance?topic=hardware-firewall-dedicated-ibm-cloud-ip-ranges
-def fetchIBMIpRanges():
-    print("Fetching IP ranges from IBM")
-
+def fetch_ibm_ip_ranges():
     url = "https://raw.githubusercontent.com/ibm-cloud-docs/hardware-firewall-dedicated/master/ips.md"
-    oracle_docs = fetchText(url)
+    oracle_docs = fetch_text(url)
     
     ipv4_ranges = []
     for line in oracle_docs.splitlines():
         for prefix in re.finditer(r'(\d{1,3}(\.\d{1,3}){3}/\d{1,2})', line):
             ipv4_ranges.append(prefix.group(0))
 
-    
-    ipv6_ranges = []
-
-    print(f'Found prefixes IPv4: {len(ipv4_ranges)} IPv6: {len(ipv6_ranges)}')
-
-    ipv4_ranges = unique_sorted(ipv4_ranges)
-    ipv6_ranges = unique_sorted(ipv6_ranges)
-
     return {
         "name": "ibm",
         "ipv4_prefixes": ipv4_ranges,
-        "ipv6_prefixes": ipv6_ranges
+        "ipv6_prefixes": []
     }
 
 
 # Oracle https://docs.cloud.oracle.com/iaas/Content/General/Concepts/addressranges.htm
-def fetchOracleCloudIpRanges():
-    print("Fetching IP ranges from Oracle Cloud")
-
+def fetch_oracle_cloud_ip_ranges():
     url = "https://docs.cloud.oracle.com/iaas/Content/General/Concepts/addressranges.htm"
-    oracle_page = fetchPage(url)
+    oracle_page = fetch_page(url)
     soup = BeautifulSoup(oracle_page, 'html.parser')
 
     items = soup.find_all('div', class_='uk-accordion-content')
@@ -246,38 +183,53 @@ def fetchOracleCloudIpRanges():
 
     prefixes = [li.get_text().split()[0] for li in prefix_li]
 
-    ipv4_ranges = prefixes
-    ipv6_ranges = [ ]
-
-    print(f'Found prefixes IPv4: {len(ipv4_ranges)} IPv6: {len(ipv6_ranges)}')
-
-    ipv4_ranges = unique_sorted(ipv4_ranges)
-    ipv6_ranges = unique_sorted(ipv6_ranges)
-
     return {
         "name": "oracle",
-        "ipv4_prefixes": ipv4_ranges,
-        "ipv6_prefixes": ipv6_ranges
+        "ipv4_prefixes": prefixes,
+        "ipv6_prefixes": []
     }
 
 
-CLOUDS = {
-    "aws": fetchAwsIpRanges,
-    "azure": fetchAzureIpRanges,
-    "cloudflare": fetchCloudflareIpRanges,
-    "gce": fetchGceIpRanges,
-    "oracle": fetchOracleCloudIpRanges,
-    "ibm": fetchIBMIpRanges
+FETCHER_BY_NAME = {
+    "aws": fetch_aws_ip_ranges,
+    "azure": fetch_azure_ip_ranges,
+    "cloudflare": fetch_cloudflare_ip_ranges,
+    "gce": fetch_gce_ip_ranges,
+    "oracle": fetch_oracle_cloud_ip_ranges,
+    "ibm": fetch_ibm_ip_ranges
 }
 
 
 def cloud_fetchers(selected_clouds):
-    selected_clouds = selected_clouds if selected_clouds else CLOUDS.keys()
+    selected_clouds = selected_clouds if selected_clouds else FETCHER_BY_NAME.keys()
     for cloud in selected_clouds:
-        if cloud not in CLOUDS.keys():
+        if cloud not in FETCHER_BY_NAME.keys():
             raise NameError(f'Unknown cloud {cloud}')
 
-    return [CLOUDS[cloud] for cloud in selected_clouds]
+    return unique_sorted(selected_clouds)
+
+
+def run_fetcher(name, ipv4, ipv6):
+    fetcher = FETCHER_BY_NAME[name]
+    print(f'Fetching IP ranges from {name}')
+    try:
+        ip_ranges = fetcher()
+        ip_ranges['timestamp'] = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+
+        (ipv4_ranges, ipv6_ranges) =  (ip_ranges['ipv4_prefixes'], ip_ranges['ipv6_prefixes'])
+
+        ipv4_ranges = unique_sorted(ipv4_ranges) if ipv4 else []
+        ipv6_ranges = unique_sorted(ipv6_ranges) if ipv6 else []
+
+        print(f'Found prefixes IPv4: {len(ipv4_ranges)} IPv6: {len(ipv6_ranges)}')
+
+        ip_ranges['ipv4_prefixes'] = ipv4_ranges
+        ip_ranges['ipv6_prefixes'] = ipv6_ranges
+
+        return ip_ranges
+    except Exception as err:
+        print(f'Error occurred fetching ip for {name} reason: {err}')
+        pass
 
 
 def main(args):
@@ -285,21 +237,16 @@ def main(args):
 
     (ipv4, ipv6) = (args['--ipv4'], args['--ipv6']) if args['--ipv4'] or args['--ipv6'] else (True, True)
 
-    sets = []
-    for fetcher in fetchers:
-        ip_ranges = fetcher()
-        ip_ranges['timestamp'] = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
-        if not ipv4:
-            ip_ranges['ipv4_prefixes'] = []
-        if not ipv6:
-            ip_ranges['ipv6_prefixes'] = []
-        
+    ranges = []
+    for name in fetchers:
+        ip_ranges = run_fetcher(name, ipv4, ipv6)
+
         if args['--verbose']:
             print(ip_ranges)
-        sets.append(ip_ranges)
+        ranges.append(ip_ranges)
 
     file_object = open(args['--output'], 'w')
-    json.dump({ "ranges": sets}, file_object, indent=4)
+    json.dump({ "ranges": ranges}, file_object, indent=4)
 
 
 if __name__ == '__main__':
