e7146f5479e9add7d7f71bae262baacc4b6ce9e2
Grzegorz Ćwiak <grzegorz.cwiak@crosswordcybersecurity.com>
2019-10-22 19:57:49 +0200

    - enable component scan on nixer-plugin-core classes exclusively.

--- core/src/main/java/eu/xword/nixer/nixerplugin/core/NixerAutoConfiguration.java
+++ core/src/main/java/eu/xword/nixer/nixerplugin/core/NixerAutoConfiguration.java
@@ -5,11 +5,11 @@ import javax.sql.DataSource;
 import eu.xword.nixer.nixerplugin.core.login.jdbc.JdbcDAO;
 import eu.xword.nixer.nixerplugin.core.registry.GlobalCredentialStuffingRegistry;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import(value = {FilterConfiguration.class, StigmaConfiguration.class})
+@ComponentScan
 public class NixerAutoConfiguration {
 
     @Bean
