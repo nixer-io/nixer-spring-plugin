04f2f90e316267fbb5f6fe928e24d092ddc3b390
Grzegorz Ćwiak <grzegorz.cwiak@crosswordcybersecurity.com>
2019-12-10 17:44:27 +0100

    - split StigmaTokenValidator test class extracting individual test classes for EncryptedJwtValidator and StigmaTokenPayloadValidator

--- core/src/test/java/io/nixer/nixerplugin/core/stigma/token/validation/StigmaTokenValidatorTest.java
+++ core/src/test/java/io/nixer/nixerplugin/core/stigma/token/validation/StigmaTokenValidatorTest.java
@@ -1,89 +1,63 @@
 package io.nixer.nixerplugin.core.stigma.token.validation;
 
-import java.io.File;
-import java.io.IOException;
 import java.text.ParseException;
-import java.time.Duration;
-import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.Date;
-import java.util.function.Consumer;
 import java.util.stream.Stream;
-import javax.annotation.Nonnull;
 
-import com.google.common.collect.Iterables;
-import com.nimbusds.jose.EncryptionMethod;
-import com.nimbusds.jose.JOSEException;
-import com.nimbusds.jose.JWEAlgorithm;
-import com.nimbusds.jose.JWEHeader;
-import com.nimbusds.jose.JWEObject;
-import com.nimbusds.jose.Payload;
-import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.OctetSequenceKey;
-import com.nimbusds.jose.jwk.gen.OctetSequenceKeyGenerator;
-import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jwt.JWT;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.PlainJWT;
-import io.nixer.nixerplugin.core.stigma.crypto.DirectDecrypterFactory;
-import io.nixer.nixerplugin.core.stigma.crypto.DirectEncrypterFactory;
 import io.nixer.nixerplugin.core.stigma.domain.RawStigmaToken;
 import io.nixer.nixerplugin.core.stigma.domain.Stigma;
-import io.nixer.nixerplugin.core.stigma.token.EncryptedStigmaTokenProvider;
-import io.nixer.nixerplugin.core.stigma.token.StigmaTokenProvider;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.Mockito;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static io.nixer.nixerplugin.core.stigma.token.StigmaTokenConstants.STIGMA_VALUE_FIELD_NAME;
 import static io.nixer.nixerplugin.core.stigma.token.StigmaTokenConstants.SUBJECT;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.verifyNoInteractions;
+import static org.mockito.Mockito.verify;
 
 /**
  * Created on 2019-05-22.
  *
  * @author gcwiak
  */
+@ExtendWith(MockitoExtension.class)
 class StigmaTokenValidatorTest {
 
     private static final Date ISSUE_TIME = Date.from(LocalDateTime.of(2019, 5, 19, 12, 45, 56).toInstant(ZoneOffset.UTC));
-    private static final Duration TOKEN_LIFETIME = Duration.ofDays(30);
-    private static final Instant NOW = LocalDateTime.of(2019, 5, 20, 13, 50, 15).toInstant(ZoneOffset.UTC);
     private static final Stigma STIGMA = new Stigma("random-stigma-value");
 
-    private OctetSequenceKey jwk;
+    @Mock
+    private JwtValidator jwtValidator;
 
-    private StigmaTokenValidator validator;
+    @Captor
+    private ArgumentCaptor<JWT> jwtCaptor;
 
-    /**
-     * FIXME This test class covers EncryptedJwtValidator and StigmaTokenPayloadValidator as well and should be split.
-     */
-
-    @BeforeEach
-    void setUp() throws Exception {
-        JWKSet jwkSet = JWKSet.load(new File("src/test/resources/stigma-jwk.json"));
-        jwk = (OctetSequenceKey) Iterables.getOnlyElement(jwkSet.getKeys());
-
-        final JwtValidator jwtValidator = new EncryptedJwtValidator(
-                new DirectDecrypterFactory(new ImmutableJWKSet(jwkSet)),
-                new StigmaTokenPayloadValidator(() -> NOW, TOKEN_LIFETIME)
-        );
-
-        validator = new StigmaTokenValidator(jwtValidator);
-    }
+    @InjectMocks
+    private StigmaTokenValidator stigmaTokenValidator;
 
     @ParameterizedTest
     @MethodSource("missingTokenExamples")
     void should_fail_validation_on_missing_token(final RawStigmaToken missingToken) {
         // when
-        final ValidationResult result = validator.validate(missingToken);
+        final ValidationResult result = stigmaTokenValidator.validate(missingToken);
 
         // then
         assertThat(result.getStatus()).isEqualTo(ValidationStatus.MISSING);
+        verifyNoInteractions(jwtValidator);
     }
 
     static Stream<RawStigmaToken> missingTokenExamples() {
@@ -96,230 +70,35 @@ class StigmaTokenValidatorTest {
     }
 
     @Test
-    void should_pass_validation() {
+    void should_pass_validation() throws ParseException {
         // given
-        final RawStigmaToken stigmaToken = givenEncryptedToken(
-                with -> with.subject(SUBJECT)
-                        .issueTime(ISSUE_TIME)
-                        .claim(STIGMA_VALUE_FIELD_NAME, STIGMA.getValue())
-        );
+        final JWTClaimsSet givenClaims = new JWTClaimsSet.Builder()
+                .subject(SUBJECT)
+                .issueTime(ISSUE_TIME)
+                .claim(STIGMA_VALUE_FIELD_NAME, STIGMA.getValue()).build();
+
+        final JWT jwt = new PlainJWT(givenClaims);
+        final RawStigmaToken stigmaToken = new RawStigmaToken(jwt.serialize());
+
+        given(jwtValidator.validate(any(JWT.class))).willReturn(ValidationResult.valid(STIGMA));
 
         // when
-        final ValidationResult result = validator.validate(stigmaToken);
+        final ValidationResult result = stigmaTokenValidator.validate(stigmaToken);
 
         // then
         assertThat(result).isEqualTo(ValidationResult.valid(STIGMA));
-    }
 
-    @Test
-    void should_fail_validation_on_incorrect_encryption_method() {
-        // given
-        final PlainJWT plainJWT = new PlainJWT(new JWTClaimsSet.Builder()
-                .subject(SUBJECT)
-                .issueTime(ISSUE_TIME)
-                .claim(STIGMA_VALUE_FIELD_NAME, STIGMA.getValue())
-                .build());
-        final StigmaTokenProvider plainTokenProvider = Mockito.mock(StigmaTokenProvider.class);
-        given(plainTokenProvider.getToken(STIGMA)).willReturn(plainJWT);
-
-        final EncryptedStigmaTokenProvider encryptedTokenProvider = new EncryptedStigmaTokenProvider(
-                plainTokenProvider,
-                new DirectEncrypterFactory(this.jwk) {
-                    @Nonnull
-                    @Override
-                    public EncryptionMethod getEncryptionMethod() {
-                        return EncryptionMethod.A256GCM;
-                    }
-                }
-        );
-
-        final RawStigmaToken stigmaToken = new RawStigmaToken(encryptedTokenProvider.getToken(STIGMA).serialize());
-
-        // when
-        final ValidationResult result = validator.validate(stigmaToken);
-
-        // then
-        assertThat(result.getStatus()).isEqualTo(ValidationStatus.WRONG_ENC);
-    }
-
-    @Test
-    void should_fail_validation_on_incorrect_encryption_algorithm() throws IOException, ParseException {
-        // given
-        JWKSet jwkSet = JWKSet.load(new File("src/test/resources/stigma-jwk.json"));
-        jwk = (OctetSequenceKey) Iterables.getOnlyElement(jwkSet.getKeys());
-
-        final JwtValidator jwtValidator = new EncryptedJwtValidator(
-                new DirectDecrypterFactory(new ImmutableJWKSet(jwkSet)) {
-                    @Nonnull
-                    @Override
-                    public JWEAlgorithm getAlgorithm() {
-                        return JWEAlgorithm.A128KW;
-                    }
-                },
-                new StigmaTokenPayloadValidator(() -> NOW, TOKEN_LIFETIME)
-        );
-
-        validator = new StigmaTokenValidator(jwtValidator);
-
-        final RawStigmaToken stigmaToken = givenEncryptedToken(
-                with -> with.subject(SUBJECT)
-                        .issueTime(ISSUE_TIME)
-                        .claim(STIGMA_VALUE_FIELD_NAME, STIGMA.getValue())
-        );
-
-        // when
-        final ValidationResult result = validator.validate(stigmaToken);
-
-        // then
-        assertThat(result.getStatus()).isEqualTo(ValidationStatus.WRONG_ALG);
-    }
-
-    @Test
-    void should_fail_validation_on_not_encrypted_token() {
-        // given
-        final PlainJWT plainJWT = new PlainJWT(new JWTClaimsSet.Builder()
-                .subject(SUBJECT)
-                .issueTime(ISSUE_TIME)
-                .claim(STIGMA_VALUE_FIELD_NAME, STIGMA.getValue())
-                .build());
-
-        // when
-        final ValidationResult result = validator.validate(new RawStigmaToken(plainJWT.serialize()));
-
-        // then
-        assertThat(result.getStatus()).isEqualTo(ValidationStatus.NOT_ENCRYPTED);
-    }
-
-    @Test
-    void should_fail_validation_on_incorrect_subject() {
-        // given
-        final RawStigmaToken stigmaToken = givenEncryptedToken(
-                with -> with.subject("INVALID_SUBJECT")
-                        .issueTime(ISSUE_TIME)
-                        .claim(STIGMA_VALUE_FIELD_NAME, STIGMA.getValue())
-        );
-
-        // when
-        final ValidationResult result = validator.validate(stigmaToken);
-
-        // then
-        assertThat(result.getStatus()).isEqualTo(ValidationStatus.INVALID_PAYLOAD);
-    }
-
-    @Test
-    void should_fail_validation_on_missing_random_stigma() {
-        // given
-        final RawStigmaToken stigmaToken = givenEncryptedToken(
-                with -> with.subject(SUBJECT)
-                        .issueTime(ISSUE_TIME)
-        );
-
-        // when
-        final ValidationResult result = validator.validate(stigmaToken);
-
-        // then
-        assertThat(result.getStatus()).isEqualTo(ValidationStatus.MISSING_STIGMA);
-    }
-
-    @Test
-    void should_fail_validation_on_missing_issue_time() {
-        // given
-        final RawStigmaToken stigmaToken = givenEncryptedToken(
-                with -> with.subject(SUBJECT)
-                        .claim(STIGMA_VALUE_FIELD_NAME, STIGMA.getValue())
-        );
-
-        // when
-        final ValidationResult result = validator.validate(stigmaToken);
-
-        // then
-        assertThat(result.getStatus()).isEqualTo(ValidationStatus.INVALID_PAYLOAD);
-    }
-
-    @Test
-    void should_fail_validation_on_expired_token() {
-        // given
-        final Date expiredIssueTime = Date.from(ISSUE_TIME.toInstant().minus(TOKEN_LIFETIME.plusDays(1)));
-
-        final RawStigmaToken stigmaToken = givenEncryptedToken(
-                with -> with.subject(SUBJECT)
-                        .issueTime(expiredIssueTime)
-                        .claim(STIGMA_VALUE_FIELD_NAME, STIGMA.getValue())
-        );
-
-        // when
-        final ValidationResult result = validator.validate(stigmaToken);
-
-        // then
-        assertThat(result.getStatus()).isEqualTo(ValidationStatus.EXPIRED);
+        verify(jwtValidator).validate(jwtCaptor.capture());
+        assertThat(jwtCaptor.getValue().getJWTClaimsSet()).isEqualTo(givenClaims);
     }
 
     @Test
     void should_fail_validation_on_parsing_error() {
         // when
-        final ValidationResult result = validator.validate(new RawStigmaToken("not parsable JWE"));
+        final ValidationResult result = stigmaTokenValidator.validate(new RawStigmaToken("not parsable JWE"));
 
         // then
         assertThat(result.getStatus()).isEqualTo(ValidationStatus.PARSING_ERROR);
     }
 
-    @Test
-    void should_fail_validation_on_payload_parsing_error() throws JOSEException {
-        // given
-        final Payload payload = new Payload("not parsable payload");
-
-        final DirectEncrypterFactory encryptionSpec = new DirectEncrypterFactory(this.jwk);
-
-        final JWEHeader header = new JWEHeader.Builder(encryptionSpec.getAlgorithm(), encryptionSpec.getEncryptionMethod())
-                .keyID(encryptionSpec.getKeyId()).build();
-
-        final JWEObject jweObject = new JWEObject(header, payload);
-        jweObject.encrypt(encryptionSpec.encrypter());
-
-        final RawStigmaToken stigmaToken = new RawStigmaToken(jweObject.serialize());
-
-        // when
-        final ValidationResult result = validator.validate(stigmaToken);
-
-        // then
-        assertThat(result.getStatus()).isEqualTo(ValidationStatus.PAYLOAD_PARSING_ERROR);
-    }
-
-    @Test
-    void should_fail_validation_on_decryption_error() throws JOSEException {
-        // given
-        final OctetSequenceKey anotherKeyWithMatchingID = new OctetSequenceKeyGenerator(256).keyID(jwk.getKeyID()).generate();
-
-        final RawStigmaToken stigmaToken = givenEncryptedToken(anotherKeyWithMatchingID,
-                with -> with.subject(SUBJECT)
-                        .issueTime(ISSUE_TIME)
-                        .claim(STIGMA_VALUE_FIELD_NAME, STIGMA.getValue())
-        );
-
-        // when
-        final ValidationResult result = validator.validate(stigmaToken);
-
-        // then
-        assertThat(result.getStatus()).isEqualTo(ValidationStatus.DECRYPTION_ERROR);
-    }
-
-    private RawStigmaToken givenEncryptedToken(final OctetSequenceKey encryptionKey, final Consumer<JWTClaimsSet.Builder> claimsAssigner) {
-        final JWTClaimsSet.Builder builder = new JWTClaimsSet.Builder();
-        claimsAssigner.accept(builder);
-        final PlainJWT plainJWT = new PlainJWT(builder.build());
-
-        final StigmaTokenProvider plainTokenProvider = Mockito.mock(StigmaTokenProvider.class);
-        given(plainTokenProvider.getToken(STIGMA)).willReturn(plainJWT);
-
-        final EncryptedStigmaTokenProvider encryptedTokenProvider =
-                new EncryptedStigmaTokenProvider(plainTokenProvider, new DirectEncrypterFactory(encryptionKey));
-
-        final JWT token = encryptedTokenProvider.getToken(STIGMA);
-
-        return new RawStigmaToken(token.serialize());
-    }
-
-    private RawStigmaToken givenEncryptedToken(final Consumer<JWTClaimsSet.Builder> claimsAssigner) {
-        return givenEncryptedToken(this.jwk, claimsAssigner);
-    }
 }
