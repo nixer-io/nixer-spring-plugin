77131412c9dde9fab54088a8793c13b11bb20db0
Grzegorz Ćwiak <grzegorz.cwiak@crosswordcybersecurity.com>
2020-01-22 16:58:26 +0100

    refactor(core): NML-23 Use NowSource in CredentialStuffingRegistry, refactor tests for readability and time control.

--- core/src/test/java/io/nixer/nixerplugin/core/detection/registry/CredentialStuffingRegistryTest.java
+++ core/src/test/java/io/nixer/nixerplugin/core/detection/registry/CredentialStuffingRegistryTest.java
@@ -1,46 +1,71 @@
 package io.nixer.nixerplugin.core.detection.registry;
 
+import java.time.Duration;
 import java.time.Instant;
-import java.time.temporal.ChronoUnit;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 import io.nixer.nixerplugin.core.detection.events.GlobalCredentialStuffingEvent;
-import io.nixer.nixerplugin.core.detection.events.GlobalCredentialStuffingEvent;
+import io.nixer.nixerplugin.core.util.NowSource;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static java.time.temporal.ChronoUnit.SECONDS;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doReturn;
 
+@ExtendWith(MockitoExtension.class)
 class CredentialStuffingRegistryTest {
 
-    private final CredentialStuffingRegistry registry = new CredentialStuffingRegistry();
+    private static final Instant CS_EVENT_OCCURRENCE_TIME = LocalDateTime.of(2019, 5, 20, 13, 50, 15).toInstant(ZoneOffset.UTC);
+    private static final Instant CS_EVENT_HANDLING_TIME = CS_EVENT_OCCURRENCE_TIME.plus(1, SECONDS);
+
+    private static final Duration CS_DURATION = Duration.ofMinutes(15);
+
+    @Spy
+    private GlobalCredentialStuffingEvent credentialStuffingEvent = new GlobalCredentialStuffingEvent();
+
+    @Mock
+    private NowSource nowSource;
+
+    private CredentialStuffingRegistry registry;
+
+    @BeforeEach
+    void setUp() {
+        doReturn(CS_EVENT_OCCURRENCE_TIME.toEpochMilli()).when(credentialStuffingEvent).getTimestamp();
+
+        given(nowSource.currentTimeMillis()).willReturn(CS_EVENT_HANDLING_TIME.toEpochMilli());
+
+        registry = new CredentialStuffingRegistry(CS_DURATION, nowSource);
+    }
 
     @Test
     void shouldReturnActiveOnEvent() {
-        registry.onApplicationEvent(new GlobalCredentialStuffingEvent());
+        final long beforeCredentialStuffingExpires = CS_EVENT_OCCURRENCE_TIME.plus(CS_DURATION).minus(1, SECONDS).toEpochMilli();
 
-        final boolean isActive = registry.hasHappenDuringCredentialStuffing(now());
+        handleCSEvent();
+        final boolean isActive = registry.hasHappenDuringCredentialStuffing(beforeCredentialStuffingExpires);
 
         assertTrue(isActive);
     }
 
     @Test
     void credentialStuffingShouldExpire() {
-        registry.onApplicationEvent(new GlobalCredentialStuffingEvent());
+        final long afterCredentialStuffingExpired = CS_EVENT_OCCURRENCE_TIME.plus(CS_DURATION).plus(1, SECONDS).toEpochMilli();
 
-        final boolean isActive = registry.hasHappenDuringCredentialStuffing(future());
+        handleCSEvent();
+        final boolean isActive = registry.hasHappenDuringCredentialStuffing(afterCredentialStuffingExpired);
 
         assertFalse(isActive);
     }
 
-    private long future() {
-        return Instant.now()
-                .plus(1, ChronoUnit.DAYS)
-                .toEpochMilli();
+    private void handleCSEvent() {
+        registry.onApplicationEvent(credentialStuffingEvent);
     }
-
-    private long now() {
-        return Instant.now().toEpochMilli();
-    }
-
-
 }
