b6cb41508220f45c052296db61ab2306995930db
Grzegorz Ćwiak <grzegorz.cwiak@crosswordcybersecurity.com>
2020-02-19 11:01:46 +0100

    refactor(integration-test): Improve naming of basic system assumptions test class.

--- samples/example/src/integration-test/java/io/nixer/example/integrationTest/basis/BasicSystemAssumptionsTest.java
+++ samples/example/src/integration-test/java/io/nixer/example/integrationTest/basis/BasicSystemAssumptionsTest.java
@@ -34,7 +34,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @EnableAutoConfiguration(exclude = InfluxMetricsExportAutoConfiguration.class)
-public class SystemFoundationTest {
+public class BasicSystemAssumptionsTest {
 
     @Autowired
     private MockMvc mockMvc;
