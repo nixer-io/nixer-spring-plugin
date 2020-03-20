c8b02f9686f56eed6a12bdb4c4a9f08d89846342
Grzegorz Ćwiak <grzegorz.cwiak@crosswordcybersecurity.com>
2020-03-20 18:00:35 +0100

    docs(core): Document properties of Ip-range filters and behavior execution.

--- core/src/main/java/io/nixer/nixerplugin/core/detection/filter/ip/IpFilterProperties.java
+++ core/src/main/java/io/nixer/nixerplugin/core/detection/filter/ip/IpFilterProperties.java
@@ -5,8 +5,15 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "nixer.filter.ip")
 public class IpFilterProperties {
 
+    /**
+     * Whether matching requests IP addresses to defined IP ranges is enabled.
+     */
     private boolean enabled;
 
+    /**
+     * Location of file resource with the IP ranges.
+     * Can be either a "classpath:" pseudo URL, a "file:" URL, or a plain file path.
+     */
     private String ipPrefixesPath;
 
     public boolean isEnabled() {
