4b4e716b495fa2fa6be99ae5adb18aeaab14907b
Grzegorz Ćwiak <grzegorz.cwiak@crosswordcybersecurity.com>
2020-03-20 17:35:41 +0100

    docs(core): Update event logging props description.

--- core/src/main/java/io/nixer/nixerplugin/core/detection/events/log/EventLogProperties.java
+++ core/src/main/java/io/nixer/nixerplugin/core/detection/events/log/EventLogProperties.java
@@ -5,6 +5,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "nixer.events.log")
 public class EventLogProperties {
 
+    /**
+     * Whether anomaly events logging is enabled.
+     */
     private boolean enabled;
 
     public boolean isEnabled() {
