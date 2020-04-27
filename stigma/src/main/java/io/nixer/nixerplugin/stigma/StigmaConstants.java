5289c0b355bb5cd05b5f2bb78b39b063b176cc58
Grzegorz Ćwiak <grzegorz.cwiak@crosswordcybersecurity.com>
2020-04-27 17:32:46 +0200

    fix(docs): Use bare literals rather than constants for Stigma properties default values so they are properly rendered in spring-configuration-metadata.json for documentation purposes.

--- stigma/src/main/java/io/nixer/nixerplugin/stigma/StigmaConstants.java
+++ stigma/src/main/java/io/nixer/nixerplugin/stigma/StigmaConstants.java
@@ -12,10 +12,6 @@ public abstract class StigmaConstants {
     public static final String SUBJECT = "stigma-token";
     public static final String STIGMA_VALUE_FIELD_NAME = "stigma-value";
 
-    public static final String DEFAULT_STIGMA_COOKIE_NAME = "stgtk";
-
-    public static final Duration DEFAULT_STIGMA_LIFETIME = Duration.ofDays(365);
-
     public static final String STIGMA_METADATA_ATTRIBUTE = "nixer.stigma.metadata";
 
     private StigmaConstants() {
